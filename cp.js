988312c18df62d606df7a22bc628ea405e49b42e
nikkiricks <nikki.ricks@gmail.com>
2019-10-26 09:47:26 +1100

    the start of code poetry

---
+++ cp.js
@@ -0,0 +1,11 @@
+
+
+
+var great = "actually great"
+var men = "men"
+
+if ( men === "great" ) {
+  men = !great
+} else {
+  men === great
+}
---
+++ lonely.js
@@ -0,0 +1,33 @@
+
+// inspired by Eleanor Rigby lyrics 
+
+function universe (x, y) {
+  var world = []
+  for (i=0; i<y; i++) {
+    var eachLine = []
+    for (j=0; j<x; j++) {
+      eachLine.push("PPL")
+    }
+    world.push(eachLine)
+  }
+  console.log(world)
+
+  var lonelyWorld = []
+
+  for (i=0; i<y; i++) {
+    var eachLine = []
+    for (j=0; j<x; j++) {
+      eachLine.push("LP")
+    }
+    lonelyWorld.push(eachLine)
+  }
+  console.log(lonelyWorld)
+}
+universe(5, 5)
+
+
+
+
+  // var randRowIndex = Math.floor(Math.random()*row)
+  // var randColIndex = Math.floor(Math.random()*col)
+  // world[randColIndex][randRowIndex] = "Treasure"
